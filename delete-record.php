67fbd8d36f3f06719b1ef94d99e72421640bf362
belgort-clark <belgort@clark.edu>
2021-02-09 08:01:06 -0800

    Updated Delete Code

--- delete-record.php
+++ delete-record.php
@@ -6,14 +6,15 @@ require_once __DIR__ . "/inc/app/config.inc.php";
 // check to see if id is in the query string
 if (isset($_GET["id"])) {
     // Build SQL for delete with placeholder for id
-    $sql = "DELETE FROM STUDENT WHERE id=:id LIMIT 1";
+    $sql = "DELETE FROM $db_table WHERE id=:id LIMIT 1";
     // Prepare the statement
     $stmt = $db->prepare($sql);
     // Execute the query
     $stmt->execute(["id" => $_GET["id"]]);
     // If you get a row back you successfully deleted the record
-    $result = $stmt->rowCount();
-    if ($result) {
+    $stmt->fetch();
+
+    if ($stmt->rowCount() == 1) {
         // if succesfuly, redirect the user to the display-records.php page with a parameter
         header('location: display-records.php?message=I%20 successfully%20deleted%20that%20record%20for%20you.');
     } else {
